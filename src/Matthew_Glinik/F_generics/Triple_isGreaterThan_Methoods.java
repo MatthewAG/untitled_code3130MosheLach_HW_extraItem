d6b6ddb0724667985da68c8798f12cc607ab3d25
MatthewAG <matthew.glinik51@bcmail.cuny.edu>
2025-11-20 21:31:46 -0500

    Thu_Nov_20_8_24_PM

--- src/Matthew_Glinik/F_generics/Triple_isGreaterThan_Methoods.java
+++ src/Matthew_Glinik/F_generics/Triple_isGreaterThan_Methoods.java
@@ -3,4 +3,22 @@ package Matthew_Glinik.F_generics;
 public interface Triple_isGreaterThan_Methoods<E> {
     // 1115
     public boolean isGreaterThan(E other);
+    // 3115
+    /*
+    public default boolean greaterThan_s(E other){
+        return(false);
+    }
+    */
+    /*
+    public default boolean greaterThan_s(Object o){
+        if(other instanceof E)
+        {
+            return(greaterThan_s((E) other));
+        }
+        else
+        {
+            return(false);
+        }
+    }
+    */
 }
--- src/Matthew_Glinik/F_generics/Triple_isLessThan_Methoods.java
+++ src/Matthew_Glinik/F_generics/Triple_isLessThan_Methoods.java
@@ -2,5 +2,23 @@ package Matthew_Glinik.F_generics;
 
 public interface Triple_isLessThan_Methoods<E> {
     // 1115
-    public boolean isGreaterThan(E other);
+    public boolean isLessThan(E other);
+    // 3115
+    /*
+    public default boolean lessThan_s(E other){
+        return(false);
+    }
+    */
+    /*
+    public default boolean lessThan_s(Object o){
+        if(other instanceof E)
+        {
+            return(lessThan_s((E) other));
+        }
+        else
+        {
+            return(false);
+        }
+    }
+    */
 }
