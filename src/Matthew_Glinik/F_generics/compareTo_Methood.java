91ee12df7d28fffc41dbeed22f10f492cb21f9de
MatthewAG <matthew.glinik51@bcmail.cuny.edu>
2025-11-20 21:45:21 -0500

    Thu_Nov_20_8_24_PM

--- src/Matthew_Glinik/F_generics/compareTo_Methood.java
+++ src/Matthew_Glinik/F_generics/compareTo_Methood.java
@@ -1,4 +1,38 @@
 package Matthew_Glinik.F_generics;
 
 public interface compareTo_Methood<E> extends Triple_isEqualTo_Methoods<E>, Triple_isGreaterThan_Methoods<E>, Triple_isLessThan_Methoods<E>, Comparable<E> {
+    // 1115
+    boolean isEqualTo(E other);
+    boolean isGreaterThan(E other);
+    boolean isLessThan(E other);
+    // 3130
+    /*
+    public default int compareTo(E o)
+    {
+        if(equals(o))
+        {
+            return(0);
+        }
+        else if(greaterThan_s(o))
+        {
+            return(1);
+        }
+        else if(lessThan_s(o))
+        {
+            return(-1);
+        }
+
+        int return_value = getComparasionNumber(o);
+        if((return_value) != (0))
+        {
+            return(return_value);
+        }
+        else
+        {
+            return(getFilterNumber(o));
+        }
+    }
+    */
+    public int getComparasionNumber(E o);
+    public int getFilterNumber(E o);
 }
